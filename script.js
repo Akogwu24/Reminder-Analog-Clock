26f69a96cc1523ffc4d379e0ec8b3010486d7e26
Akogwu Emmanuel <akogwu.emmanuel24@gmail.com>
2021-03-23 21:17:14 +0100

    syntax corrected

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const HOURHAND = document.querySelector('#hour');
-const MINUTEHAND = document.querySelector('#minute');
-const SECONDHAND = document.querySelector('#second');
+const hourHand = document.querySelector('#hour');
+const minuiteHand = document.querySelector('#minute');
+const secondHand = document.querySelector('#second');
 
 function runTheClock() {
   //get the current date at this very moment
@@ -14,9 +14,9 @@ function runTheClock() {
   let minPosition = (minuite * 360) / 60 + (seconds * (360 / 60)) / 60;
   let secPosition = (seconds * 360) / 60;
 
-  HOURHAND.style.transform = 'rotate(' + hrPosition + 'deg)';
-  MINUTEHAND.style.transform = 'rotate(' + minPosition + 'deg)';
-  SECONDHAND.style.transform = 'rotate(' + secPosition + 'deg)';
+  hourHand.style.transform = 'rotate(' + hrPosition + 'deg)';
+  minuiteHand.style.transform = 'rotate(' + minPosition + 'deg)';
+  secondHand.style.transform = 'rotate(' + secPosition + 'deg)';
 
   if (hrPosition % 30 === 0) {
     let notification = document.body.firstElementChild;
@@ -27,14 +27,4 @@ function runTheClock() {
   }
 }
 
-// runTheClock();
 setInterval(runTheClock, 1000);
-
-// function ringTheAlarm() {
-//   let notification = document.querySelector('.main').firstElementChild;
-//   const message = 'Hour clock Reminder.....go and review your todo list';
-//   notification.textContent = message;
-//   const alarm = new Audio('alarmSound.mp3');
-//   alarm.play();
-// }
-// setInterval(ringTheAlarm, 360000);
